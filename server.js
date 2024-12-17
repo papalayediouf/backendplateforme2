13a0b1565672ef36e9ccd29fd1c543c4519555ea
Papa Abdoulaye Diouf <papalayediouf17@gmail.com>
2024-12-17 16:56:35 +0000

    2 commit

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
-const formationRoutes = require('./routes/formationRoutes');
-const path = require('path');
+const formationRoutes = require('./routes/formationRoutes'); 
 
 connectDB();
 
@@ -11,16 +10,12 @@ const app = express();
 
 // Middleware
 app.use(cors());
-app.use(express.json()); // Pour les requêtes JSON
-
-// Servir les fichiers téléchargés depuis le dossier 'uploads'
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
+app.use(express.json()); 
 // Routes
-app.use('/api/formations', formationRoutes);
+app.use(formationRoutes);  
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Serveur démarré sur le port ${PORT}`);
+  console.log(`Serveur demarre sur le port ${PORT}`);
 });
